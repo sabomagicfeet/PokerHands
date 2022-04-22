bb664f9f1ae50f0b66d569409bd7b82f35157389
sabo <sabo@jihyes-mbp.home>
2022-04-22 10:44:37 +0100

    add putFiveCarsIntoHashMapTest to change the card String input into HashMap

--- src/main/java/Poker.java
+++ src/main/java/Poker.java
@@ -1,4 +1,11 @@
+import java.util.HashMap;
+
 public class Poker {
+    private static final Integer JACK = 11;
+    private static final Integer QUEEN = 12;
+    private static final Integer KING = 13;
+    private static final Integer ACE = 14;
+
 
     public String compareSimpleNumbers(int black, int white) {
         if(black > white) {
@@ -7,4 +14,34 @@ public class Poker {
             return "White wins. - with high card: " + white;
         }
     }
+
+    public HashMap<Integer, String> putFiveCardsIntoHashMap(String fiveCards) {
+        HashMap<Integer, String> cardHashMap = new HashMap();
+
+        // change J,Q,K,A to integer values
+        // put each card string to HashMap <Integer, String>
+        for( String eachCard : fiveCards.split(" ") ) {
+            if(eachCard.charAt(0) == 'J') {
+                cardHashMap.put(JACK, String.valueOf(eachCard.charAt(1)) );
+            } else if(eachCard.charAt(0) == 'Q') {
+                cardHashMap.put(QUEEN, String.valueOf(eachCard.charAt(1)) );
+            } else if(eachCard.charAt(0) == 'K') {
+                cardHashMap.put(KING, String.valueOf(eachCard.charAt(1)) );
+            } else if(eachCard.charAt(0) == 'A') {
+                cardHashMap.put(ACE, String.valueOf(eachCard.charAt(1)) );
+            } else {
+                cardHashMap.put(Integer.parseInt( String.valueOf(eachCard.charAt(0)) ), String.valueOf(eachCard.charAt(1)) );
+            }
+        }
+
+        return cardHashMap;
+
+    }
+
+//    public String findHighCard(String fiveCards) {
+//        HashMap<Integer, String> cardsHashMap = putFiveCardsIntoHashMap(fiveCards);
+//        List<>
+//
+//        return "";
+//    }
 }
--- src/test/java/PokerGameTest.java
+++ src/test/java/PokerGameTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 public class PokerGameTest {
 
     @Test
@@ -13,4 +15,51 @@ public class PokerGameTest {
         Assertions.assertEquals(expected, actual);
 
     }
+
+    @Test
+    public void putFiveCardsIntoHashMapTest() {
+        Poker poker = new Poker();
+
+        HashMap<Integer, String> expectedHashMap = new HashMap<>();
+        expectedHashMap.put(2, "H");
+        expectedHashMap.put(3, "D");
+        expectedHashMap.put(5, "S");
+        expectedHashMap.put(9, "C");
+        expectedHashMap.put(4, "D");
+
+        HashMap actualHashMap = poker.putFiveCardsIntoHashMap("2H 3D 5S 9C 4D");
+
+        Assertions.assertEquals(expectedHashMap,actualHashMap);
+    }
+
+    @Test
+    public void putFiveCardsWithJQKAIntoHashMapTest() {
+        Poker poker = new Poker();
+
+        HashMap<Integer, String> expectedHashMap = new HashMap<>();
+        expectedHashMap.put(2, "H");
+        expectedHashMap.put(11, "D");
+        expectedHashMap.put(12, "S");
+        expectedHashMap.put(13, "C");
+        expectedHashMap.put(14, "D");
+
+        HashMap actualHashMap = poker.putFiveCardsIntoHashMap("2H JD QS KC AD");
+
+        Assertions.assertEquals(expectedHashMap,actualHashMap);
+    }
+
+//    @Test
+//    public void findHighCardTest() {
+//        Poker poker = new Poker();
+//
+//        String expected = "9C";
+//        String actual = poker.findHighCard("2H 3D 5S 9C 4D");
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
+//
+//    @Test
+//    public void compareStringTest() {
+//
+//    }
 }
